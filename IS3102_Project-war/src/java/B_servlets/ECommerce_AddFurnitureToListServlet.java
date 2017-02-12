f2c270c3b27e65986168badc982fbd2400266be8
Enidtingz <Enidtingz@DIT-NB1529926>
2017-02-12 16:22:04 +0800

    ECommerce_AddFurnitureToListServlet Allow + quantity of same item

--- IS3102_Project-war/src/java/B_servlets/ECommerce_AddFurnitureToListServlet.java
+++ IS3102_Project-war/src/java/B_servlets/ECommerce_AddFurnitureToListServlet.java
@@ -5,16 +5,12 @@
  */
 package B_servlets;
 
-import HelperClasses.Furniture;
 import HelperClasses.ShoppingCartLineItem;
 import java.io.IOException;
-import java.io.PrintWriter;
 import static java.lang.System.out;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,49 +19,92 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
  *
- * @author User
+ * @author Lim
  */
+@WebServlet(name = "ECommerce_AddFurnitureToListServlet", urlPatterns = {"/ECommerce_AddFurnitureToListServlet"})
 public class ECommerce_AddFurnitureToListServlet extends HttpServlet {
-    
-    
 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-           try {
-             HttpSession session = request.getSession();
-             Long countryID=(Long) session.getAttribute("countryID");
-             
-             ShoppingCartLineItem cart=new ShoppingCartLineItem();
-             cart.setId(request.getParameter("id"));
-             cart.setSKU(request.getParameter("SKU"));
-             cart.setName(request.getParameter("name"));
-             cart.setImageURL(request.getParameter("imageURL"));
-             cart.setPrice(Double.parseDouble(request.getParameter("price")));
-             cart.setQuantity(1);
-             
-             cart.setCountryID(countryID);
-             
-             ArrayList<ShoppingCartLineItem> Shoppingcart=new ArrayList<ShoppingCartLineItem>();
-             Shoppingcart.add(cart);
-             
-             session.setAttribute("shoppingCart",Shoppingcart);
-                response.sendRedirect("/IS3102_Project-war/B/SG/shoppingCart.jsp?goodMsg=Item Successfully added into the cart!");
 
-           }
-           catch (Exception ex) {
+        try {
+            HttpSession session = request.getSession();
+            Long countryID = (Long) session.getAttribute("countryID");
+            ArrayList<ShoppingCartLineItem> current = (ArrayList<ShoppingCartLineItem>) (session.getAttribute("shoppingCart"));
+            ArrayList<ShoppingCartLineItem> Shoppingcart = new ArrayList<ShoppingCartLineItem>();
+            
+            ShoppingCartLineItem cart = new ShoppingCartLineItem();
+            
+            cart.setId(request.getParameter("id"));
+            cart.setSKU(request.getParameter("SKU"));
+            cart.setName(request.getParameter("name"));
+            cart.setImageURL(request.getParameter("imageURL"));
+            cart.setPrice(Double.parseDouble(request.getParameter("price")));
+            cart.setCountryID(countryID);
+            cart.setQuantity(+1);
+          
+            Shoppingcart.add(cart);    
+            
+            if (current != null) {
+
+                for (ShoppingCartLineItem items : current) {
+                    
+                    if(items.getSKU().equals(cart.getSKU()))
+                    {
+                        Shoppingcart.remove(cart);
+                        items.setQuantity(items.getQuantity()+1);
+                        Shoppingcart.add(items);
+                    }                 
+                    else
+                    {
+                        Shoppingcart.add(items);
+                    }
+                }
+                
+            }
+            session.setAttribute("shoppingCart", Shoppingcart);
+            response.sendRedirect("/IS3102_Project-war/B/SG/shoppingCart.jsp?goodMsg=Item Successfully added into the cart!");
+        } catch (Exception ex) {
             out.println("\n\n " + ex.getMessage());
-           }
+        }
     }
-           
 
-   
+    /*public int getQuantityOfSKU(Long CountryID, String SKU) {
+        try {
+            Client client = ClientBuilder.newClient();
+            WebTarget target = client
+                    .target("http://localhost:8080/WebServices/webresources/entity.countryentity/")
+                    .path("getQuantity")
+                    .queryParam("CountryID", CountryID)
+                    .queryParam("SKU", SKU);
+            Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+            Response response = invocationBuilder.get();
+            System.out.println("status: " + response.getStatus());
+
+            String result = (String) response.readEntity(String.class);
+            System.out.println("Result returned from ws: " + result);
+            return Integer.parseInt(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+
+    }*/
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
